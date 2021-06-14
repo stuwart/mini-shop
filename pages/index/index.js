f1e872bf5d6d201448f71bf0db3353cc455e87d8
unknown <unknown@example.com>
2021-06-14 20:03:31 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,44 @@
+Page(
+ 
+  {
+    data: {
+    //轮播图
+    swiperList:[]
+  },
+  
+  //options(Object)
+  onLoad: function(options) {
+    //1.发送异步请求获取轮播图数据
+    
+      
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
+  }
+});
+  
